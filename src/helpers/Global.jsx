b0c5376c47a4c19f5cec4c5a766ce14a986a0bea
BlackSpirit1201 <wilsonpedraza69@gmail.com>
2024-11-12 20:53:07 -0500

    all changes

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,12 @@
+// Ruta del Backend desplegado
+  export const Global = {
+    url: "https://b3-backend-social-net-4fmf.onrender.com"
+  }
+
+
+// Ruta del Backend en Local: se debe ejecutar el servidor del backend
+/*
+export const Global = {
+    url: "http://localhost:3900/api/"
+  }
+*/
